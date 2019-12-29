bbc3aa931f31e286e51ccb05a7fbc30a1a50dd51
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2019-12-29 12:49:16 +0100

    Initial commit, create empty project

---
+++ Simple Login/Simple Login/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Simple Login
+//
+//  Created by Thanh-Nhon Nguyen on 29/12/2019.
+//  Copyright © 2019 SimpleLogin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
